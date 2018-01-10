62a2cea3972f0f3e99d2c51a40018d39e66e53c9
dansbands <dansbands@hotmail.com>
2018-01-10 11:51:50 -0500

    dryish

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -3,25 +3,54 @@ import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
+	constructor() {
+		super();
 
+		this.state = {
+			active: 'profile'
+		}
+	}
+
+	handleChange = (event) => {
+		console.log(event);
+		this.setState({
+			active: event.target.id,
+		})
+	}
 
 	render (){
+		console.log(this.state);
+		/*
 
-		/* 
-
-		Replace the code below! Depending on what menu item is selected in the menu, 
-		I should render either a Profile, Photos, Cocktails, or Pokemon 
-		component.Think of a way to track which menu item was selected. 
-		Which component should have state? Which component should have 
+		Replace the code below! Depending on what menu item is selected in the menu,
+		I should render either a Profile, Photos, Cocktails, or Pokemon
+		component.Think of a way to track which menu item was selected.
+		Which component should have state? Which component should have
 		methods to control state? Where should these methods be called?
 
 		*/
+		let detailsToDisplay = <div>Hi, I'm a div!</div>
 
-		let detailsToDisplay = <div>Hi, I'm a div!</div> 
+		if (this.state.active === 'profile') {
+			detailsToDisplay = <Profile />
+		}
+		else if (this.state.active === 'photo') {
+			detailsToDisplay = <Photos />
+		}
+		else if (this.state.active === 'cocktail') {
+			detailsToDisplay = <Cocktails />
+		}
+		else if (this.state.active === 'pokemon') {
+			detailsToDisplay = <Pokemon />
+		}
+
 
 		return (
 		<div>
-			<MenuBar />
+			<MenuBar
+				activateButton={this.handleChange}
+				active={this.state.active}
+			/>
 			{detailsToDisplay}
 		</div>
 		)
